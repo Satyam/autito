e43a0ea461bca17475291ec73d99ea91c16ae03a
Satyam <satyam@satyam.com.ar>
2020-01-28 17:04:27 +0100

    Improved on arrow drawing

--- src/Arrow.tsx
+++ src/Arrow.tsx
@@ -6,33 +6,65 @@ export enum DIRS {
   LEFT,
 }
 
-const Arrow: React.FC<{ dir: DIRS, width: number, length: number, value: number, cx: number, cy: number }> = ({ dir, width, length, value, cx, cy }) => {
-  let v = 0;
-  let a = 0;
-  switch (dir) {
-    case DIRS.UP:
-      a = 180;
-      if (value > 0) v = value;
-      break;
-    case DIRS.RIGHT:
-      a = 270;
-      if (value > 0) v = value;
-      break;
-    case DIRS.LEFT:
-      a = 90;
-      if (value < 0) v = -value;
-      break;
-    case DIRS.DOWN:
-      if (value < 0) v = -value;
-      break;
-  }
-  return (
-    <g transform={`rotate(${a},${cx},${cy})`}>
-      <rect x={cx - width / 3} y={width} width={width * 2 / 3} height={length} stroke="black" fill="none" />
-      {v && (<rect x={cx - width / 3} y={width} width={width * 2 / 3} height={v} stroke="black" fill="green" />)}
+const Arrow: React.FC<{
+  dir?: DIRS,
+  width?: number,
+  length?: number,
+  gap?: number,
+  arrowWidth?: number,
+  arrowTip?: number,
+  value?: number,
+  cx?: number,
+  cy?: number
+}> = ({
+  dir = DIRS.UP,
+  width = 25,
+  length = 100,
+  gap = 30,
+  arrowWidth = 50,
+  arrowTip = 50,
+  value = 0,
+  cx = 0,
+  cy = 0,
+}) => {
+    let v = 0;
+    let a = 0;
+    switch (dir) {
+      case DIRS.UP:
+        a = 180;
+        if (value < 0) v = -value;
+        break;
+      case DIRS.RIGHT:
+        a = -90;
+        if (value > 0) v = value;
+        break;
+      case DIRS.LEFT:
+        a = 90;
+        if (value < 0) v = -value;
+        break;
+      case DIRS.DOWN:
+        if (value > 0) v = value;
+        break;
+    }
+    return (
+      <g transform={`rotate(${a},${cx},${cy})`}>
+        <path
+          d={`
+            M ${cx - width / 2} ${cy + gap}
+            h ${width}
+            v ${length}
+            h ${(arrowWidth - width) / 2}
+            L 0 ${cy + gap + length + arrowTip}
+            L ${cx - arrowWidth / 2} ${cy + gap + length}
+            h ${(arrowWidth - width) / 2}
+            Z`}
+          stroke="black"
+          fill="none"
+        />
+        {v && (<rect x={cx - width / 2} y={cy + gap} width={width} height={v * length / 255} fill="green" />)}
 
-    </g>
-  )
-}
+      </g>
+    )
+  }
 
 export default Arrow;
--- src/Knob.tsx
+++ src/Knob.tsx
@@ -1,45 +1,57 @@
 import React, { useState } from 'react';
 import Arrow, { DIRS } from './Arrow';
 
-const Knob: React.FC<{ remote: boolean, x: number, y: number, width: number, height: number }> = ({ remote, x, y, width, height }) => {
-  const [isDragging, setDragging] = useState<boolean>(false);
-  const [initial, setInitial] = useState<[number, number]>([0, 0]);
-  const [pos, setPos] = useState<[number, number]>([0, 0]);
+const Knob: React.FC<{
+  remote: boolean,
+  x: number,
+  y: number,
+  width: number,
+  height: number
+}> = ({
+  remote,
+  x,
+  y,
+  width,
+  height
+}) => {
+    const [isDragging, setDragging] = useState<boolean>(false);
+    const [initial, setInitial] = useState<[number, number]>([0, 0]);
+    const [pos, setPos] = useState<[number, number]>([0, 0]);
 
-  const mouseDownHandler = (ev: React.MouseEvent<SVGCircleElement>) => {
-    setInitial([ev.clientX, ev.clientY]);
-    setPos([0, 0])
-    setDragging(true);
-  }
-  const mouseMoveHandler = (ev: React.MouseEvent<SVGSVGElement>) => {
-    if (isDragging) {
-      setPos([Math.round((ev.clientX - initial[0]) / width * 512), Math.round((ev.clientY - initial[1]) / height * 512)]);
-      console.log(Math.round((ev.clientX - initial[0]) / width * 512), Math.round((ev.clientY - initial[1]) / height * 512));
+    const mouseDownHandler = (ev: React.MouseEvent<SVGCircleElement>) => {
+      setInitial([ev.clientX, ev.clientY]);
+      setPos([0, 0])
+      setDragging(true);
     }
-  }
-  const mouseUpHandler = (ev: React.MouseEvent<SVGSVGElement>) => {
-    setDragging(false);
-  }
-  return (
-    <svg
-      width={width}
-      height={height}
-      viewBox="-300 -300 600 600"
-      xmlns="http://www.w3.org/2000/svg"
-      onMouseMove={mouseMoveHandler}
-      onMouseUp={mouseUpHandler}
-      onMouseLeave={mouseUpHandler}
-      style={{ cursor: isDragging ? 'crosshair' : 'default' }}
-    >
-      <circle cx={x} cy={-y} r="20" fill="grey" onMouseDown={mouseDownHandler} />
-      {remote && (<circle cx={x} cy={-y} r="5" fill="green" />)}
-      <Arrow dir={DIRS.UP} width={40} length={100} value={pos[1]} cx={x} cy={y} />
-      <Arrow dir={DIRS.LEFT} width={40} length={100} value={pos[0]} cx={x} cy={y} />
-      <Arrow dir={DIRS.DOWN} width={40} length={100} value={pos[1]} cx={x} cy={y} />
-      <Arrow dir={DIRS.RIGHT} width={40} length={100} value={pos[0]} cx={x} cy={y} />
+    const mouseMoveHandler = (ev: React.MouseEvent<SVGSVGElement>) => {
+      if (isDragging) {
+        setPos([Math.round((ev.clientX - initial[0]) / width * 512), Math.round((ev.clientY - initial[1]) / height * 512)]);
+        console.log(Math.round((ev.clientX - initial[0]) / width * 512), Math.round((ev.clientY - initial[1]) / height * 512));
+      }
+    }
+    const mouseUpHandler = (ev: React.MouseEvent<SVGSVGElement>) => {
+      setDragging(false);
+    }
+    return (
+      <svg
+        width={width}
+        height={height}
+        viewBox="-300 -300 600 600"
+        xmlns="http://www.w3.org/2000/svg"
+        onMouseMove={mouseMoveHandler}
+        onMouseUp={mouseUpHandler}
+        onMouseLeave={mouseUpHandler}
+        style={{ cursor: isDragging ? 'crosshair' : 'default' }}
+      >
+        <circle cx={x} cy={-y} r="20" fill="grey" onMouseDown={mouseDownHandler} />
+        {remote && (<circle cx={x} cy={-y} r="5" fill="green" />)}
+        <Arrow dir={DIRS.UP} width={40} length={100} value={pos[1]} />
+        <Arrow dir={DIRS.LEFT} width={40} length={100} value={pos[0]} />
+        <Arrow dir={DIRS.DOWN} width={40} length={100} value={pos[1]} />
+        <Arrow dir={DIRS.RIGHT} width={40} length={100} value={pos[0]} />
 
-    </svg>
-  )
-}
+      </svg>
+    )
+  }
 
 export default Knob;
