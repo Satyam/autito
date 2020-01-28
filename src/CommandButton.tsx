8b4e56406ec73c40756af8bfa8c084796aabe589
Satyam <satyam@satyam.com.ar>
2020-01-28 19:53:16 +0100

    Made useSocket().command take a simple array and turn it into an ArrayBuffer for transmission

--- src/CommandButton.tsx
+++ src/CommandButton.tsx
@@ -2,15 +2,11 @@ import React, { useCallback, } from 'react';
 import { useSocketIO } from './useSocketIO';
 
 
-const CommandButton: React.FC<{ command: ArrayBuffer }> = ({ command, children }) => {
+const CommandButton: React.FC<{ command: number[] }> = ({ command, children }) => {
   const socket = useSocketIO();
 
   const handleClick = useCallback(() => {
     if (socket) {
-      console.log('emitting', command);
-      // 'binary' is not included in the type definition so it is flagged as error, which is not.
-      // @ ts-ignore
-      // socket.binary(true).compress(false).emit('command', command);
       socket.command(command)
     }
   }, [socket, command]);
--- src/useSocketIO.tsx
+++ src/useSocketIO.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useMemo, useContext } from 'react';
 import io from 'socket.io-client';
 
 export type MySocket = (SocketIOClient.Socket & {
-  command: (command: ArrayBuffer) => MySocket;
+  command: (command: number[]) => MySocket;
 }) | null;
 
 export const SocketIOContext = createContext<MySocket>(null);
@@ -12,8 +12,14 @@ export const SocketIOProvider: React.FC<{ url?: string, options?: SocketIOClient
   const socket: MySocket = useMemo(() => {
     const s = io(url, options)
     if (s) {
+      // .command not part of SocketIOClient.Socket
       // @ts-ignore
-      s.command = (command: ArrayBuffer) => s.binary(true).compress(true).emit('command', command);
+      s.command = (command: number[]) => {
+        console.log('emitting', String.fromCodePoint(command[0]), command[1]);
+        // 'binary' is not included in the type definition so it is flagged as error, which is not.
+        // @ts-ignore
+        return s.binary(true).compress(true).emit('command', new Uint8Array(command));
+      }
 
     }
     return s as MySocket;
